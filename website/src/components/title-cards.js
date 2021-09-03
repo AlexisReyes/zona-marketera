e43a0a4f849ef7d6f58df79e0f9f0a62af194e40
Alexis Reyes <alexisreyes88@gmail.com>
2021-09-02 22:28:43 -0600

    Se modifican tarjetas de servicios

--- website/src/components/title-cards.js
+++ website/src/components/title-cards.js
@@ -3,75 +3,58 @@ import React from "react";
 function TitleCards() {
 
   return (
-    <section className="bg-white border-b py-8">
+    <section className="bg-white py-8">
       
       <div className="container mx-auto flex flex-wrap pt-4 pb-12">
       
-        <h3 className="w-full my-2 text-4xl font-bold leading-tight text-center text-gray-800">Servicios</h3>
+        <h3 className="w-full my-2 text-4xl font-bold leading-tight text-center text-gray-800">SERVICIOS</h3>
         <div className="w-full mb-4">   
-          <div className="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+          <p className="uppercase text-gray-800 text-center text-lg">
+           Diseñamos y desarrollamos estrategias creativas para cada proyecto
+          </p>
         </div>
 
         <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
           <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow ">
-            <div className="flex flex-wrap no-underline hover:no-underline">
-
-              <div className="w-full font-bold text-2xl text-center text-gray-800 my-4">Diseño</div>
-              <ul className="px-4 py-8 text-xl text-gray-700">
-                <li>- Logotipo</li>
-                <li>- Manual de identidad</li>
-                <li>- Documentos corporativos</li>
-                <li>- Animación de</li>
-              </ul>
-            </div>
-          </div>
-          <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-            <div className="flex items-center justify-start">
-              <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Action</button>
+            <div className="flex flex-wrap no-underline hover:no-underline bg-lime h-full">
+              <img className="w-full h-64" />
+              <div className="w-full font-bold text-2xl text-center my-2">Branding</div>
+              <div className="p-4 text-xl h-full">
+                Creamos conceptos unicos que CONECTEN con tu negocio.
+              </div>
             </div>
           </div>
         </div>
 
         <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
           <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-            <div className="flex flex-wrap no-underline hover:no-underline">
-
-              <div className="w-full font-bold text-2xl text-center text-gray-800 my-4 mx-2">Estrategias de marketing digital</div>
-              <ul className="px-4 py-8 text-xl text-gray-700">
-                <li>- Logotipo</li>
-                <li>- Manual de identidad</li>
-                <li>- Documentos corporativos</li>
-                <li>- Animación de</li>
-              </ul>
-            </div>
-          </div>
-          <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-            <div className="flex items-center justify-center">
-              <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Action</button>
+            <div className="flex flex-wrap no-underline hover:no-underline bg-lime">
+              <img className="w-full h-64" />
+              <div className="w-full font-bold text-2xl text-center my-2">Marketing Digital</div>
+              <div className="p-4 text-xl">
+                ¡DEJA QUE TE ENCUENTREN! estar en medios digitales ya es una necesidad.
+              </div>
             </div>
           </div>
         </div>
 
         <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
           <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow ">
-            <div className="flex flex-wrap no-underline hover:no-underline">
-
-              <div className="w-full font-bold text-2xl text-center text-gray-800 my-4">Diseño</div>
-              <ul className="px-4 py-8 text-xl text-gray-700">
-                <li>- Logotipo</li>
-                <li>- Manual de identidad</li>
-                <li>- Documentos corporativos</li>
-                <li>- Animación de</li>
-              </ul>
-            </div>
-          </div>
-          <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-            <div className="flex items-center justify-end">
-              <button className="mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg">Action</button>
+            <div className="flex flex-wrap no-underline hover:no-underline bg-lime">
+              <img className="w-full h-64" />
+              <div className="w-full font-bold text-2xl text-center my-2">Diseño Web</div>
+              <div className="p-4 text-xl">
+                Brinda credibilidad a tu marca y hazte visible en los principales buscadores.
+              </div>
             </div>
           </div>
         </div>
 
+      </div>
+      <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden">
+        <div className="flex items-center justify-center">
+          <button className="mx-auto lg:mx-0 hover:underline bg-lime text-white font-bold rounded-full my-6 py-4 px-8 uppercase tracking-widest">Conoce mas</button>
+        </div>
       </div>
 
     </section>
