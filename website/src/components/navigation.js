567fb428629c40376450e1494a0ff303fc120f03
Alexis Reyes <alexisreyes88@gmail.com>
2021-09-11 14:35:59 -0600

    Se agrega boton cerrar para menu

--- website/src/components/navigation.js
+++ website/src/components/navigation.js
@@ -20,8 +20,11 @@ function Navigation() {
 
         <div className="lg:hidden">
           <button onClick={() => toggleExpansion(!isExpanded)}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 m-2 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            <svg xmlns="http://www.w3.org/2000/svg" className={`${ isExpanded ? `hidden` : `block` } h-8 w-8 m-2 text-gray-700`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" className={`${ isExpanded ? `block` : `hidden` } h-8 w-8 m-2 text-gray-700`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
         </div>
