98a42f7b4880305c47d3d66bc26681dce0b2a3fb
Alexis Reyes <alexisreyes88@gmail.com>
2021-11-08 22:08:30 -0700

    Se completa formulario de contacto

--- website/src/components/contact-form.js
+++ website/src/components/contact-form.js
@@ -1,40 +1,65 @@
 import React, { useCallback, useEffect, useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
+import emailjs from 'emailjs-com';
 
 function ContactForm() {
 
   const [validCaptcha, setValidCaptcha] = useState(false);
   const [expired, setExpired]           = useState(false);
+
+  const [name, setName]           = useState('');
+  const [email, setEmail]         = useState('');
+  const [telephone, setTelephone] = useState('');
+  const [comments, setComments]   = useState('');
   const inputStyle = "col-span-3 text-gray-600 rounded-md p-2 focus:ring-4 focus:ring-lime focus:ring-opacity-50 outline-none";
+
+  emailjs.init(`${process.env.GATSBY_EMAILJS_USERID}`);
 
   const onChange = (value) => {
     setValidCaptcha(value);
     if (value === null) setExpired(!value);
   }
 
-  const submitForm = () => {
+  const submitForm = event => {
 
+    event.preventDefault();
+
+    const templateParams = {
+      from_name: name,
+      to_name: `${process.env.GATSBY_EMAILJS_TO_NAME}`,
+      message: comments,
+      email: email,
+      telephone: telephone
+    };
+
+    emailjs.send(`${process.env.GATSBY_EMAILJS_SERVICEID}`, `${process.env.GATSBY_EMAILJS_TEMPLATEID}`, templateParams)
+      .then((result) => {
+          console.log('SUCCESS!', result, result.status, result.text);
+      }, (error) => {
+         console.error('FAILED...', error);
+      });
   }
 
   return (
     <form onSubmit={submitForm}>
+
       <div className="grid md:grid-cols-4 grid-cols-1 gap-y-4 text-2xl font-myriad-pro mx-6">
 
         <label className="pr-6 md:text-right text-left self-center text-gray-800 text-xl">Nombre:</label>
-        <input type="text" className={inputStyle} placeholder="Nombre" required/>
+        <input type="text" className={inputStyle} value={name} onChange={(event) => setName(event.target.value)} placeholder="Nombre" required/>
 
         <label className="pr-6 md:text-right text-left self-center text-gray-800 text-xl">Correo:</label>
-        <input type="text" className={inputStyle} placeholder="Correo" required/>
+        <input type="email" className={inputStyle} value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Correo" required/>
 
         <label className="pr-6 md:text-right text-left self-center text-gray-800 text-xl">Telefono:</label>
-        <input type="text" className={inputStyle} placeholder="Telefono" required/>
+        <input type="number" className={inputStyle} value={telephone} onChange={(event) => setTelephone(event.target.value)} placeholder="Telefono" required/>
 
         <label className="pr-6 md:text-right text-left self-center text-gray-800 text-xl" >Comentarios:</label>
-        <textarea className={`${inputStyle} resize-none`} rows="6" required></textarea>
+        <textarea className={`${inputStyle} resize-none`} rows="6" value={comments} onChange={(event) => setComments(event.target.value)} required></textarea>
 
         <div className="self-center mb-4 lg:col-span-2 col-span-2">
           <ReCAPTCHA
-            sitekey="6LcwGV4cAAAAADaFkWfPgB6jVyo1ktF2juXueZxa"
+            sitekey={`${process.env.GATSBY_RECAPTCHA_KEY}`}
             onChange={onChange}>
           </ReCAPTCHA>
         </div>
